55fac6584250b8005dddcb8d7b24939dd0d0b9be
Maksim <m.glinka@rambler-co.ru>
2017-01-27 12:05:40 +0300

    Adds more lodash, cuz lodash is all we need

--- src/components/widgets/blog/ListPresenter.js
+++ src/components/widgets/blog/ListPresenter.js
@@ -3,23 +3,18 @@ import PieChartPresenter from './PieChartPresenter';
 import { map } from 'lodash/collection';
 import  React from 'react';
 const { DOM } = React;
+import { assign, pick } from 'lodash';
 
 const ListPresenter = (props) => (
       DOM.div({}, DOM.ul({},
       map(props.blogEntries, (entry) => (
-        React.createElement(Item, {
-          meta: entry.meta,
-          image: entry.image,
-          likes: entry.likes,
-          text: entry.text,
-          key: entry.id,
-          id: entry.id,
-          increaselikesHandler: props.increaseLikesHandler
-        })))),
+        React.createElement(Item,
+          assign({}, pick(entry, ['meta', 'image', 'likes', 'text', 'id']),
+            {increaselikesHandler: props.increaseLikesHandler, key: entry.id,})
+        )))),
         React.createElement(PieChartPresenter,{
           likeTitles: props.likeTitles}))
 );
-
 ListPresenter.propTypes = {
   blogEntries: React.PropTypes.array,
   increaseLikesHandler: React.PropTypes.func,
