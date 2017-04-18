1965fcf0ff1a1f7d71a7682a549951dded0d4514
Maksim <m.glinka@rambler-co.ru>
2017-04-18 17:58:28 +0300

    Text refactoring

--- src/components/widgets/blog/elements/UncontrolledForm.js
+++ src/components/widgets/blog/elements/UncontrolledForm.js
@@ -1,24 +1,61 @@
 import React, { Component } from 'react';
 import { bind } from 'lodash';
+import { mapValues } from 'lodash/object';
 
 class UncontrolledForm extends Component {
   constructor(props) {
     super(props);
+    this.form = {};
     this.handleSubmitClick = bind(this.handleSubmitClick, this);
+    this.generateRef = bind(this.generateRef , this);
   }
 
-  handleSubmitClick() {
-    console.log(this._name.value);
+  handleSubmitClick(e) {
+    e.preventDefault();
+    const values = mapValues(this.form, 'value');
+    console.log(JSON.stringify(values));
+  }
+
+  generateRef(field) {
+    return ((input) => (this.form[field] = input));
   }
 
   render() {
     return (
       <div>
-        <input type="text" ref={ input => this._name = input} />
-        <button onClick={this.handleSubmitClick}>Submit</button>
+        <form className="ui form" onSubmit={this.handleSubmitClick} >
+          <Text
+            label='Full name'
+            name='fullName'
+            fieldRef={ this.generateRef('fullName') }
+            />
+            <Text
+              label='Email'
+              name='email'
+              fieldRef={ this.generateRef('email') }
+              />
+          <input type="submit" className="ui button primary" value="Submit" />
+        </form>
       </div>
     );
   }
 }
 
 export default UncontrolledForm;
+
+class Text extends React.Component {
+  render(){
+    const { label, name, fieldRef } = this.props;
+    return (
+      <div className="ui field">
+        <label for={ name }>{ label }</label>
+        <input
+          id={ name }
+          className="ui input"
+          name={ name }
+          ref={ fieldRef }
+          />
+      </div>
+    );
+  }
+}
