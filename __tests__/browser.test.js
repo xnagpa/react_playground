5731c30989a5f6d5da31a21f1d2b4ee0a582a35c
Maksim <m.glinka@rambler-co.ru>
2017-05-17 10:08:55 +0300

    Improves issues after cr

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -10,8 +10,7 @@ describe('when visit the homepage', () => {
   it('adds like if pressed', async () => {
     let page = nightmare().goto('http://localhost:3000/posts/1');
     let likes_before = await page.evaluate(() => parseInt(document.querySelector('.content .extra div p').innerHTML));
-    console.log("BEFORE: "+likes_before);
-    
+
     let likes_after = page.click('.extra button.ui')
       .evaluate(() => parseInt(document.querySelector('.content .extra div p').innerHTML))
       .end()
@@ -20,21 +19,5 @@ describe('when visit the homepage', () => {
         done();
       })
 
-    // console.log("AFTER: "+likes_after);
-    // expect(likes_before - likes_after).toBe(1);
   }, 20000);
 });
-
-// nightmare
-//   .goto('https://duckduckgo.com')
-//   .type('#search_form_input_homepage', 'github nightmare')
-//   .click('#search_button_homepage')
-//   .wait('#zero_click_wrapper .c-info__title a')
-//   .evaluate(function () {
-//     return document.querySelector('#zero_click_wrapper .c-info__title a').href
-//   })
-//   .end()
-//   .then(function(link) {
-//     expect(link).to.equal('https://github.com/segmentio/nightmare');
-//     done();
-//   })
